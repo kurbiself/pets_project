09088f4c002d89370f31e61a935576a323a7bb44
kurbiself <kurbiself@mail.ru>
2024-10-23 20:55:22 +0700

    test changes

--- pets_project/pets_list/views.py
+++ pets_project/pets_list/views.py
@@ -7,6 +7,6 @@ from django.http import HttpResponse
 def pets_types(request):
     if request.method == 'GET':
         tmp = PetsTypes.objects.all().order_by('name') #Сортировка по имени
-        items = [str(item) for item in tmp]
+        items = [str(item) for item in tmp] #получили строковое представление объекта
 
     return HttpResponse(','.join(items))
